4d120faf9897e119109f811a50726b9f040559c7
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-13 09:06:06 +0530

    Added:To validate six digit pincode

--- PincodeValidation.sh
+++ PincodeValidation.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -x
 
-echo "Welcome to pincode validation"
+echo "Enter the Pincode"
+read Pincode
+PinCodePattern="^[0-9]{6}$"
+
+# check for pincode validation
+function validation()
+{
+   if [[ $1 =~ $2  ]]
+   then
+      echo "$1 is Valid"
+   else
+      echo "$1 is Invalid"
+   fi
+}
+
+validation $Pincode $PinCodePattern
+
